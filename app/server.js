084460b4142340df18c549f085f9009ad96b843b
Lars Nysom <LANY@ucn.dk>
2025-04-07 11:06:28 +0200

    Initial commit

---
+++ app/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = 3000;
+
+mongoose.connect('mongodb://mongo:27017/demo', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Item = mongoose.model('Item', { name: String });
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+app.post('/', async (req, res) => {
+  const item = new Item({ name: req.body.name });
+  await item.save();
+  res.status(201).json(item);
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
